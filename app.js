7f0ca94dccfa4dcc2bb37e3ae1a0a0295afb39c4
Alex Kashko <alexkrond@yahoo.com>
2020-02-07 21:32:39 +0300

    Добавлен main роут, куки и инициальзация passport

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ require('./config/passport-setup.js');
 
 const express = require('express');
 const authRouter = require('./routes/auth-routes.js');
+const mainRouter = require('./routes/main-routes.js');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const app = express();
 
 
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [process.env.COOKIE_KEY]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(
-    process.env.MONGODB_URI,
+    process.env.MONGODB_URL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -22,11 +33,11 @@ mongoose.connect(
 
 app.use(express.static('public'));
 
-// app.get('/fail', (req, res) => res.send('fail'));
-
+app.use('/', mainRouter);
 app.use('/auth', authRouter);
+
 app.use((req, res) => {
-  res.status(404).send('404');
+  res.status(404).send('404. Страницы не существует.');
 });
 
 
