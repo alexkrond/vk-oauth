ce46d34d5dbe6bf117c0c5d1980b1e8caae3cea1
Alex Kashko <alexkrond@yahoo.com>
2020-02-07 21:31:38 +0300

    Добавлена переадресация и выход

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,21 +3,27 @@ const passport = require('passport');
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-  res.send('login');
+  if(!req.user) {
+    res.render('login');
+  } else {
+    res.redirect('/');
+  }
 });
 
 router.get('/logout', (req, res) => {
-  res.send('logout');
+  req.logout();
+  res.redirect('/');
 });
 
 router.get('/vk', passport.authenticate('vkontakte', {
   scope: ['friends', 'offline']
+  // scope: ['friends', 'offline', 'email']
 }));
 
 router.get('/vk/redirect',
     passport.authenticate('vkontakte'),
     (req, res) => {
-      res.send('callback URI');
+      res.redirect('/');
     }
 );
 
