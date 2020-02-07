3267095b729519a6bfac4827927a683120428bbc
Alex Kashko <alexkrond@yahoo.com>
2020-02-07 22:56:11 +0300

    Запуск сервера после подключения к бд

--- app.js
+++ app.js
@@ -16,32 +16,29 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000,
   keys: [process.env.COOKIE_KEY]
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-
-mongoose.connect(
-    process.env.MONGODB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    },
-    () => {
-      console.log('connected to mongodb');
-    }
-);
-
 app.use(express.static('public'));
-
 app.use('/', mainRouter);
 app.use('/auth', authRouter);
-
 app.use((req, res) => {
   res.status(404).send('404. Страницы не существует.');
 });
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
-});
+(async () => {
+  await mongoose.connect(
+      process.env.MONGODB_URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+      () => {
+        console.log('connected to mongodb');
+      }
+  );
+  app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+  });
+})();
