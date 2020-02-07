a76534dec5e6142d46f86937f01993fc2142a227
Alex Kashko <alexkrond@yahoo.com>
2020-02-07 17:13:14 +0300

    Добавлено подключение к бд

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 require('dotenv').config();
+require('./config/passport-setup.js');
+
 const express = require('express');
 const authRouter = require('./routes/auth-routes.js');
-const passportSetup = require('./config/passport-setup.js');
+const mongoose = require('mongoose');
 const app = express();
 
 
 app.set('view engine', 'ejs');
 
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    () => {
+      console.log('connected to mongodb');
+    }
+);
+
 app.use(express.static('public'));
+
+// app.get('/fail', (req, res) => res.send('fail'));
+
 app.use('/auth', authRouter);
 app.use((req, res) => {
   res.status(404).send('404');
 });
 
 
-const PORT = process.env.PORT ||  5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
